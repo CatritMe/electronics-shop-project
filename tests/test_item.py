892f133e411cac8d3b865bd4731de1553f59a068
Стас naprienko ekaterina <catrit1993@gmail.com>
2023-12-12 20:22:45 +0500

    add item and tests

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,16 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+from src.item import Item
+
+
+@pytest.fixture
+def soap():
+    return Item('Soap', 50, 1000)
+
+
+def test_calculate_total_price():
+    assert soap.calculate_total_price() == 50000
+
+
+def test_apply_discount():
+    assert soap.apply_discount() == 50
